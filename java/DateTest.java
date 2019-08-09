dfc01feda60da9aad487232d427713dda661b657
design_jsi <design@jsinformatics.com>
2019-08-09 18:07:54 +0530

    Date test added.

---
+++ java/DateTest.java
@@ -0,0 +1,8 @@
+public class DateTest{
+
+	public static void main(String[] args) {
+		long timestamp = System.currentTimeMillis();
+		System.out.println(timestamp);
+		System.out.println("\n\n");
+	}
+}
